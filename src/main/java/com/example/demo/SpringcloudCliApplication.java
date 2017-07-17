ceb380d01d07804f6d69aa809d0e43a71d77f868
donghei <2125374074@qq.com>
2017-07-17 15:55:06 +0800

    进行jpa测试11

--- src/main/java/com/example/demo/SpringcloudCliApplication.java
+++ src/main/java/com/example/demo/SpringcloudCliApplication.java
@@ -1,27 +1,55 @@
 package com.example.demo;
 
+import com.example.demo.model.Cat;
+import com.example.demo.service.Catservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class SpringcloudCliApplication {
 
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 
 		SpringApplication.run(SpringcloudCliApplication.class, args);
 	}
 
-	@Value("${server.port}")
-	String port;
-	@RequestMapping(value="/hi")
-	public String sayhi(@RequestParam String name){
-		return "im is"+name+"port is"+ port+"11113335554448888";
-	}
+    @Resource
+    private Catservice catService;
+
+    @RequestMapping(value="/save")
+    public String save(){
+        Cat cat = new Cat();
+        cat.setCatName("jack");
+        cat.setCatAge(3);
+        catService.save(cat);
+        return "save ok.";
+    }
+
+    @RequestMapping(value="/delete")
+    public String delete(@RequestParam String name){
+      /*  catService.delete(1);*/
+        return "delete ok"+name;
+    }
+
+    @RequestMapping(value="/getAll")
+    public Iterable<Cat> getAll(){
+        return catService.getAll();
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping(value="/hi")
+    public String sayhi(@RequestParam String name){
+        return "im dddis"+name+"port is"+ port+"11113335554448888";
+    }
 }
